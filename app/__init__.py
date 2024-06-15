9d5ed7b0ae9c1bded9743fb588969e37251a6905
lasc95 <lasc.95@hotmail.com>
2024-06-15 14:55:05 -0400

    add paginations

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 def create_app():
     app = Flask(__name__)
@@ -6,5 +6,9 @@ def create_app():
 
     from .routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
+
+    @app.errorhandler(404)
+    def not_found_error(error):
+        return render_template('404.html'), 404
 
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
+import json
 
 main = Blueprint('main', __name__)
 
@@ -12,7 +13,19 @@ def blog():
 
 @main.route('/works')
 def works():
-    return render_template('works.html')
+    with open('app/static/data/projects_data_works.json') as file:
+        projects = json.load(file)
+    page = request.args.get('page', 1, type=int)
+    per_page = 6 # number of projects per page
+    total = len(projects)
+    projects = projects[(page - 1) * per_page: page * per_page]
+    pagination = {
+        'total': total,
+        'page': page,
+        'per_page': per_page,
+        'pages': (total + per_page - 1) // per_page,  # Número total de páginas
+    }
+    return render_template('works.html', projects=projects, pagination=pagination)
 
 @main.route('/contact')
 def contact():
